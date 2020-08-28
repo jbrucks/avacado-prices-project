8773e8f409f0a67298a81b20f664a022f2ca2145
Jake Brucks <jakobpbrucks@gmail.com>
2020-08-28 14:31:29 -0500

    logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,14 +1,15 @@
 d3.json("/api/v1.0/data").then((data)=>{
   // console.log(data)
+});
 
   // Define sections of data
-  var avo_prices = data.avocado_prices
-  var gas_prices = data.gas_prices
-  var tot_transport = data.tot_transport
-  var avo_transport = data.avo_transport
-  var weather = data.weather
-  var weather2 = data.weather2
-  var bananas = data.bananas
+  // var avo_prices = data.avocado_prices
+  // var gas_prices = data.gas_prices
+  // var tot_transport = data.tot_transport
+  // var avo_transport = data.avo_transport
+  // var weather = data.weather
+  // var weather2 = data.weather2
+  // var bananas = data.bananas
   // console.log(avo_prices)
   // console.log(gas_prices)
   // console.log(tot_transport)
@@ -16,32 +17,19 @@ d3.json("/api/v1.0/data").then((data)=>{
   // console.log(weather)
   // console.log(bananas)
 
-  // -- FUNCTION TO HIDE TITLE BLOCK --
-  $(document).ready(function(){
+// -- FUNCTION TO HIDE TITLE BLOCK --
+$(document).ready(function(){
 
-    $('#hide').click(function(){
-    $('#titleblock').toggle('slow');
+  $('#hide').click(function(){
+  $('#titleblock').toggle(1000);
     
-    });
+  });
     
-    });
-
-  // var hide = document.querySelector('#hide');
-
-  // hide.addEventListener('click', function(){
-  //   var jumbo = document.getElementById('#titleblock');
-  //   if(jumbo.style.display == 'none'){
-  //   jumbo.style.display = 'block';
-  //   }
-  //   else{
-  //   jumbo.style.display = 'none';
-  //   }
-
-  // });
-  // -- FUNCTION TO HIDE TITLE BLOCK --
+});
+// -- FUNCTION TO HIDE TITLE BLOCK --
 
   // initialize charts for rewriting with input data
-  function init() {
+function init() {
     
     // Set up chart
     var trace = {
@@ -60,20 +48,57 @@ d3.json("/api/v1.0/data").then((data)=>{
       showlegend: false,
     };
     
-    var chart = d3.selectAll("#chart").node();
+    var chart = d3.select("#chart").node();
     
     Plotly.newPlot(chart, data, layout);
-  }
+}
 
-  // -- TRANSPORT DATA FUNCTIONS AND INFO --
-  d3.selectAll(".avocado__skin").on("click", transport);
+// -- TRANSPORT DATA FUNCTIONS AND INFO --
+d3.select(".avocado__skin").on("click", transport);
 
   function transport() {
     // console.log("it worked");
 
     // replotting transport visualizations
+    
+    var avoTransportTrace1 = {
+      x: [], // tot_transport[0].date,
+      y: [], // tot_transport[0].average_weekly_rate,
+      mode: 'markers',
+      marker: {
+        size: []
+      }
+    };
 
+    var avoTransportTrace2 = {
+      x: [], // tot_transport[0].date,
+      y: [], // avo_transport[0].average_weekly_rate,
+      mode: 'markers',
+      marker: {
+        size: []
+      }
+    };
 
+    var avoTransportTrace3 = {
+      x: [], // tot_transport[0].date,
+      y: [], // avo_prices[0].average_price,
+      mode: 'markers',
+      marker: {
+        size: []
+      }
+    };
+    
+    var transportData = [avoTransportTrace1, avoTransportTrace2, avoTransportTrace3];
+    
+    var transportlayout = {
+      title: 'Transport Data Analysis',
+      showlegend: false,
+    };
+    
+    var transportChart = d3.select("#chart").node();
+    
+    Plotly.react(transportChart, transportData, transportlayout);
+    
     // grab nodes for updating info card
     var dashboard_title = d3.select("#dashboard_title");
     var dashboard_subTitle = d3.select("#dashboard_subtitle");
@@ -82,11 +107,50 @@ d3.json("/api/v1.0/data").then((data)=>{
     // Update info card with new text
     dashboard_title.text("USDA Transport Data");
     dashboard_subTitle.text("Trucking Availability and Rates");
-    dashboard_text.text("This data comes from the USDA Ag Transport site ( https://agtransport.usda.gov/ ), specifically the trucking category. This data shows the availability and rates for refrigerated trucks transporting ag commodities and we utilized this and were able to drill down to see costs associated specifically with avocados.");
+    dashboard_text.text("This data comes from the USDA Ag Transport site https://agtransport.usda.gov/, specifically the trucking category. This data shows the availability and rates for refrigerated trucks transporting ag commodities and we utilized this and were able to drill down to see costs associated specifically with avocados.");
 
-  };
-  // -- TRANSPORT DATA FUNCTIONS AND INFO --
+};
+// -- TRANSPORT DATA FUNCTIONS AND INFO --
 
-  init();
+// -- WEATHER DATA FUNCTIONS AND INFO --
+d3.select(".avocado__inner").on("click", weather);
 
-});
+  function weather() {
+    // console.log("it worked");
+
+    // replotting weather visualizations
+    
+    var weatherTrace = {
+      x: [],
+      y: [],
+      mode: 'markers',
+      marker: {
+        size: []
+      }
+    };
+    
+    var weatherData = [weatherTrace];
+    
+    var weatherLayout = {
+      title: 'Weather Data Analysis',
+      showlegend: false,
+    };
+    
+    var chart = d3.selectAll("#chart").node();
+    
+    Plotly.react(chart, weatherData, weatherLayout);
+    
+    // grab nodes for updating info card
+    var dashboard_titleW = d3.select("#dashboard_title");
+    var dashboard_subTitleW = d3.select("#dashboard_subtitle");
+    var dashboard_textW = d3.select("#dashboard_text");
+    
+    // Update info card with new text
+    dashboard_titleW.text("Open Weather API");
+    dashboard_subTitleW.text("San Diego Historical Weather");
+    dashboard_textW.text("Info on this data, limitations, etc.");
+
+};
+// -- WEATHER DATA FUNCTIONS AND INFO --
+
+init();
